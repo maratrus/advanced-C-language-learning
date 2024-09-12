42b02665fc30d3e1debc2ccc37576ec417465268
maratrus <107637541+maratrus@users.noreply.github.com>
2024-09-12 18:39:28 +0300

    Update lesson_6_1.c

--- lesson_6_1.c
+++ lesson_6_1.c
@@ -6,50 +6,50 @@ void find_first_and_last_word(char *input) {
     char *last_word_start = NULL;
     char *current = input;
 
-    // Ïðîïóñêàåì âåäóùèå ïðîáåëû
-    while (*current != '\0' && (*current & 0x20) == 0x20) { // ïðîáåë â ASCII - 0x20
+    // Пропускаем ведущие пробелы
+    while (*current != '\0' && (*current & 0x20) == 0x20) { // пробел в ASCII - 0x20
         current++;
     }
 
-    // Íàõîäèì ïåðâîå ñëîâî
+    // Находим первое слово
     first_word_start = current;
 
-    // Èùåì ïåðâîå ñëîâî
+    // Ищем первое слово
     while (*current != '\0') {
-        // Åñëè òåêóùèé ñèìâîë - íå ïðîáåë
+        // Если текущий символ - не пробел
         if ((*current & 0x20) != 0x20) {
             if (last_word_start == NULL) {
-                last_word_start = current; // Íàõîäèì íà÷àëî ñëîâà
+                last_word_start = current; // Находим начало слова
             }
         } else {
-            // Ïðèõîäèòñÿ íà ïðîáåë, çíà÷èò, ñëîâî çàêîí÷èëîñü
+            // Приходится на пробел, значит, слово закончилось
             if (last_word_start) {
-                *current = '\0'; // Çàâåðøàåì ñëîâî
+                *current = '\0'; // Завершаем слово
                 break;
             }
         }
         current++;
     }
 
-    // Åñëè îñòàëîñü ïîñëåäíåå ñëîâî
+    // Если осталось последнее слово
     if (last_word_start && *last_word_start != '\0') {
-        // Èùåì êîíåö ïîñëåäíåãî ñëîâà
+        // Ищем конец последнего слова
         current = last_word_start;
         while (*current != '\0') {
             if ((*current & 0x20) == 0x20) {
-                *current = '\0'; // Çàâåðøèòü ïîñëåäíåå ñëîâî
+                *current = '\0'; // Завершить последнее слово
                 break;
             }
             current++;
         }
     }
 
-    // Ïå÷àòàåì ïåðâîå è ïîñëåäíåå ñëîâà
+    // Печатаем первое и последнее слова
     if (first_word_start && *first_word_start != '\0') {
-        printf("Ïåðâîå ñëîâî: %s\n", first_word_start);
+        printf("Первое слово: %s\n", first_word_start);
     }
     if (last_word_start && *last_word_start != '\0') {
-        printf("Ïîñëåäíåå ñëîâî: %s\n", last_word_start);
+        printf("Последнее слово: %s\n", last_word_start);
     }
 }
 
@@ -57,10 +57,10 @@ int main() {
     char input1[100] = "  efg abcd zzz.  ";
     char input2[100] = " fffff kkkkkk a. ";
 
-    printf("Äàííûå íà âõîäå: '%s'\n", input1);
+    printf("Данные на входе: '%s'\n", input1);
     find_first_and_last_word(input1);
 
-    printf("Äàííûå íà âõîäå: '%s'\n", input2);
+    printf("Данные на входе: '%s'\n", input2);
     find_first_and_last_word(input2);
 
     return 0;
